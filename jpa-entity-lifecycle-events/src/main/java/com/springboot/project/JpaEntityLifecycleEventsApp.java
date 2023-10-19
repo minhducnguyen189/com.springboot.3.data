b77e3c927db5e111dd86e3c1b9cd6ca049515b6c
Duc Nguyen <minhducnguyen189@gmail.com>
2023-10-19 23:20:30 +0700

    add example for jpa entity lifecycle events

---
+++ jpa-entity-lifecycle-events/src/main/java/com/springboot/project/JpaEntityLifecycleEventsApp.java
@@ -0,0 +1,11 @@
+package com.springboot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEntityLifecycleEventsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaEntityLifecycleEventsApp.class, args);
+    }
+}
