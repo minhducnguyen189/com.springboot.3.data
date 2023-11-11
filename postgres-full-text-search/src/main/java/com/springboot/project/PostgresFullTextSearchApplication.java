f4bf9c371ad1dc1216f65a31ae0bca4d3cdf3281
Duc Nguyen <minhducnguyen189@gmail.com>
2023-11-11 15:34:26 +0700

    add topic postgres with full text search

---
+++ postgres-full-text-search/src/main/java/com/springboot/project/PostgresFullTextSearchApplication.java
@@ -0,0 +1,11 @@
+package com.springboot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresFullTextSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresFullTextSearchApplication.class, args);
+    }
+}
---
+++ postgres-full-text-search/src/main/java/com/springboot/project/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.springboot.project.repository;
+
+
+import com.springboot.project.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
+
+    Optional<CustomerEntity> findCustomerByEmail(String email);
+
+}
