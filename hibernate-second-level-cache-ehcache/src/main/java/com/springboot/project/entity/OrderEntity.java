76a50e87581a6cb72a851dfabdb6f1bea65a26ed
Duc Nguyen <minhducnguyen189@gmail.com>
2024-02-10 11:40:53 +0700

    update ehcache configuration

--- hibernate-second-level-cache-ehcache/src/main/java/com/springboot/project/entity/OrderEntity.java
+++ hibernate-second-level-cache-ehcache/src/main/java/com/springboot/project/entity/OrderEntity.java
@@ -1,10 +1,23 @@
 package com.springboot.project.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.springframework.cache.annotation.Cacheable;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -15,7 +28,7 @@ import java.util.UUID;
 @Setter
 @Entity
 @Table(name = "orders")
-@Cacheable("MY_CACHE")
+@Cacheable
 @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class OrderEntity {
 
